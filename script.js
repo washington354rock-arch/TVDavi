4f298182e8e4d59d01348e82fd7c6775ce12ac8f
washington354rock-arch <washington354rock@gmail.com>
2025-11-26 22:19:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('tvDavi carregado!');
+fetch('noticias.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('noticias-container');
+    data.forEach(noticia => {
+      const artigo = document.createElement('article');
+      artigo.classList.add('noticia');
+      artigo.innerHTML = `
+        <h3>${noticia.titulo}</h3>
+        <p>${noticia.conteudo}</p>
+      `;
+      container.appendChild(artigo);
+    });
+  })
+  .catch(err => console.error('Erro ao carregar notícias:', err));
